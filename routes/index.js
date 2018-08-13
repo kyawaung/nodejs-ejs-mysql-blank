e592ae631255de818db34ac1c998d17871d9d1f2
kyawaung <naylinnoo322@gmail.com>
2018-08-13 14:25:17 +0630

    Add Login using Ajax

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,12 @@ router.post('/signup', function(req, res, next) {
   User.findByEmail(req.body.email, function(err, rows){
     if(err) throw err;
     if(rows.length > 0){
-      req.flash('warn', 'Duplicated email!!');
+      req.flash('warning', 'Duplicated email!!');
       res.redirect('/signup');
     }else{
       User.add(params, function(err2, result){
         if(err2) throw err2;
-        res.redirect('/signin');
+        res.render('commons/sign-up-success', { title: 'Signup success' });
       });
     }
   })
@@ -44,6 +44,38 @@ router.post('/dupemail', function(req, res, next) {
 /* GET home page. */
 router.get('/signin', function(req, res, next) {
   res.render('commons/sign-in', { title: 'Signin' });
+});
+
+/* GET home page. */
+router.post('/signin', function(req, res, next) {
+  // make model using req.body
+  User.findByEmail(req.body.email, function(err, users){
+    if(err) throw err;
+    if(users.length == 0 || !User.compare(req.body.password, users[0].password)){
+      req.flash('warning', 'Email not exists or password not matched!!');
+      res.redirect('/signin');
+    }else{
+      req.session.user = {nid: users[0].nid, name: users[0].name, email: users[0].email, role: users[0].role};
+      res.redirect('/');
+    }// user exists
+  });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('commons/login');
+});
+
+router.post('/login', function(req, res, next) {
+  User.findByEmail(req.body.email, function(err, users){
+    if(err) throw err;
+    if(users.length == 0 || !User.compare(req.body.password, users[0].password)){
+      res.json({ status: false, msg:'Email not exists or password not matched!!'});
+    }else{
+      req.session.user = {nid: users[0].nid, name: users[0].name, email: users[0].email, role: users[0].role};
+      res.json({ status: true});
+    }
+  })
 });
 
 module.exports = router;
