c6b3ff7894d0de717a70ff24081aa7b7568169d4
kyawaung <naylinnoo322@gmail.com>
2018-08-13 16:49:10 +0630

    Add Authorization

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+/*  admin role check. */
+router.use('/', function(req, res, next) {
+  if(req.session.user.role == 'ADMIN'){
+    next();
+  }else{
+    req.flash('warning', 'Not allowed user! Please login with admin');
+    res.redirect('/signin'); // redirect to other page.
+  }
+});
+
 /* GET admin home. */
 router.get('/', function(req, res, next) {
   res.render('admin/home');
--- routes/members/index.js
+++ routes/members/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET admin home. */
+/*  admin role check. */
+router.use('/', function(req, res, next) {
+  if(req.session.user.role == 'USER'){
+    next();
+  }else{
+    req.flash('warning', 'Not allowed user! Please login');
+    res.redirect('/signin'); // redirect to other page.
+  }
+});
+
+/* GET members home. */
 router.get('/', function(req, res, next) {
   res.render('members/home');
 });
